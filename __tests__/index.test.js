d724b07cedd18f87e948f5b35272f1cedb3a3f04
Sergei Shinkarenko <serewja@gmail.com>
2020-07-14 23:07:39 +0500

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,41 @@
-import f from "../dist/index.js"
+import isBracketStructureBalanced from "../dist/index.js"
 
-test('testname', () => {
-    
+test('should be balanced', () => {
+  const str = '()';
+  expect(isBracketStructureBalanced(str)).toBe(true);
+
+  const str2 = '[()]';
+  expect(isBracketStructureBalanced(str2)).toBe(true);
+
+  const str3 = '({}[])';
+  expect(isBracketStructureBalanced(str3)).toBe(true);
+
+  const str4 = '(<><<{[()]}>>)';
+  expect(isBracketStructureBalanced(str4)).toBe(true);
+
+  const str5 = '';
+  expect(isBracketStructureBalanced(str5)).toBe(true);
+});
+
+test('should not be balanced', () => {
+  const str1 = '((';
+  expect(isBracketStructureBalanced(str1)).toBe(false);
+
+  const str2 = '[[()]';
+  expect(isBracketStructureBalanced(str2)).toBe(false);
+
+  const str3 = '({}}[]';
+  expect(isBracketStructureBalanced(str3)).toBe(false);
+
+  const str4 = '(<><<{[()]}>>>)';
+  expect(isBracketStructureBalanced(str4)).toBe(false);
+
+  const str5 = '}';
+  expect(isBracketStructureBalanced(str5)).toBe(false);
+
+  const str6 = '([)]';
+  expect(isBracketStructureBalanced(str6)).toBe(false);
+
+  const str7 = '([))';
+  expect(isBracketStructureBalanced(str7)).toBe(false);
 });
